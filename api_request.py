ba243c0dbf740ec674dc08687556a9e25274643c
Mohammed Alam <ma3655@rit.edu>
2022-08-27 04:01:37 -0400

    Python Terminal Controls

--- api_request.py
+++ api_request.py
@@ -1,3 +1,4 @@
+from requests.auth import HTTPBasicAuth
 import requests
 
 
@@ -13,9 +14,51 @@ def get_character_name_by_id(id):
     return data['name']
 
 
+def get_character_name_by_location(id):
+    baseurl = "https://rickandmortyapi.com/api/"
+
+    endpoint = "location/" + str(id)
+
+    r = requests.get(baseurl + endpoint)
+
+    data = r.json()
+
+    return data['name']
+
+
+def get_character_name_by_episode(id):
+    baseurl = "https://rickandmortyapi.com/api/"
+
+    endpoint = "location/" + str(id)
+
+    r = requests.get(baseurl + endpoint)
+
+    data = r.json()
+
+    return data['name']
+
+
 def main():
-    user_id_input = input("Enter a characted Id: ")
-    print(get_character_name_by_id(user_id_input))
+
+    intro_text = """
+     1. Get Character Data\n
+     2. Get Location Data\n
+     3. Get Episode Data\t
+    """
+    user_id_input = input(intro_text)
+
+    if(user_id_input == "1"):
+        character_id_input = input("What is the Character Id? \n")
+        print("Your Character is" +
+              get_character_name_by_id(character_id_input))
+    elif(user_id_input == "2"):
+        location_id_input = input("What is the Location Id? \n")
+        print("Your Character is" +
+              get_character_name_by_location(location_id_input))
+    elif(user_id_input == "3"):
+        episode_id_input = input("What is the Episode Id? \n")
+        print("Your Character is" +
+              get_character_name_by_episode(episode_id_input))
 
 
 main()
