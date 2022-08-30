fc6adddba2650988877a1847fa099aa325df861c
Mohammed Alam <mohammedal2144@gmail.com>
2022-08-30 04:22:44 -0400

    Added note for macbook

--- api_request.py
+++ api_request.py
@@ -1,4 +1,5 @@
-from requests.auth import HTTPBasicAuth
+# To run files you have to do `python3 "directory/file.py`
+# In this case run `python3 "rmapi/api_request.py"`
 import requests
 
 
@@ -47,15 +48,15 @@ def main():
     """
     user_id_input = input(intro_text)
 
-    if(user_id_input == "1"):
+    if (user_id_input == "1"):
         character_id_input = input("What is the Character Id? \n")
         print("Your Character is" +
               get_character_name_by_id(character_id_input))
-    elif(user_id_input == "2"):
+    elif (user_id_input == "2"):
         location_id_input = input("What is the Location Id? \n")
         print("Your Character is" +
               get_character_name_by_location(location_id_input))
-    elif(user_id_input == "3"):
+    elif (user_id_input == "3"):
         episode_id_input = input("What is the Episode Id? \n")
         print("Your Character is" +
               get_character_name_by_episode(episode_id_input))
