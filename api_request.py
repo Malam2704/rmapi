e211d0cb3663461f5b745b21445447086aef65df
Mohammed Alam <ma3655@rit.edu>
2023-03-06 02:46:57 -0500

    clean up code and python prints

--- api_request.py
+++ api_request.py
@@ -36,29 +36,3 @@ def get_character_name_by_episode(id):
     data = r.json()
 
     return data['name']
-
-
-def main():
-
-    intro_text = """
-     1. Get Character Data\n
-     2. Get Location Data\n
-     3. Get Episode Data\t
-    """
-    user_id_input = input(intro_text)
-
-    if(user_id_input == "1"):
-        character_id_input = input("What is the Character Id? \n")
-        print("Your Character is" +
-              get_character_name_by_id(character_id_input))
-    elif(user_id_input == "2"):
-        location_id_input = input("What is the Location Id? \n")
-        print("Your Character is" +
-              get_character_name_by_location(location_id_input))
-    elif(user_id_input == "3"):
-        episode_id_input = input("What is the Episode Id? \n")
-        print("Your Character is" +
-              get_character_name_by_episode(episode_id_input))
-
-
-main()
